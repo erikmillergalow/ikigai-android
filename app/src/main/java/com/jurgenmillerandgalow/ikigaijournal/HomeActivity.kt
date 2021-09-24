0d535803d6e2d88e4edfa5b0f5d4edd3bef9069f
Erik Miller-Galow <erikmillergalow@gmail.com>
2021-09-24 01:23:21 -0400

    Sign in / sign out and initial nav setup

--- app/src/main/java/com/jurgenmillerandgalow/ikigaijournal/HomeActivity.kt
+++ app/src/main/java/com/jurgenmillerandgalow/ikigaijournal/HomeActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,15 +15,15 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun setupUI() {
-        sign_out_button.setOnClickListener {
-            signOut()
-        }
+//        sign_out_button.setOnClickListener {
+//            signOut()
+//        }
     }
 
-    private fun signOut() {
-        startActivity(MainActivity.getLaunchIntent(this))
-        FirebaseAuth.getInstance().signOut()
-    }
+//    private fun signOut() {
+//        startActivity(MainActivity.getLaunchIntent(this))
+//        FirebaseAuth.getInstance().signOut()
+//    }
 
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, HomeActivity::class.java).apply {
